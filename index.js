61039609eacc8baa7f87797bb19d69c8423cec12
Scott Atkins <satkins16@gmail.com>
2017-10-22 14:16:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Barry Allen';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
